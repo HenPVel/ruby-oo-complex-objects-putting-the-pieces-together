7e1b699bc53d43e91dc9da4ed05a23f59867b71d
Henry <hvelas93@gmail.com>
2021-02-23 16:49:22 -0500

    commit tree 5b85b4bf166c4415df485e75eb7298891a524b3e

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,48 @@
+# In this tutorial, our goal is to create a simple Book class. 
+# We'll want it to have properties like a title, an author, a 
+# page count, etc. We'd also like the class to somehow be able 
+# to keep track of all of the genres of all of the Books we create. 
+# Finally, we'll give our books the total non-sensical ability to 
+# turn their own pages.
 
+class Book
+
+    def initialize(title)
+        @title=title
+    end
+
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author=author
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(num)
+        @page_count=num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre=genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+        
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,53 @@
 # Make your shoe class here!
+class Shoe
+
+    def initialize(brand)
+        @brand=brand
+    end
+
+    attr_reader :brand
+    attr_accessor :color,:size,:material,:condition
+
+    def cobble
+
+        puts "Your shoe is as good as new!"
+        @condition="new"
+        #condition=new
+        # if @condition==old
+        #     @condition=new
+
+        # end
+            
+    end
+    
+    
+
+    # attr_reader :brand,:size,:material,:condition
+ 
+
+    # def initialize(brand)
+    #     @brand=brand
+    # end
+
+    # def brand
+    #     @brand
+    # end
+
+    # def color=(color)
+    #     @color=color
+    # end
+
+    # def color
+    #     @color
+    # end
+
+    # def size=(size)
+    #     @size=size
+    # end
+
+    # def size
+    #     @size
+    # end
+
+end
+
